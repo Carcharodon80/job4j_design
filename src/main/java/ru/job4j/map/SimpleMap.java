3db528b0ff6650cf7d78d69272ffb454eb45e866
Carcharodon80 <karna241280@gmail.com>
2021-07-20 23:05:39 +0300

    8. Реализовать собственную структуру данных - HashMap [#1008]

---
+++ src/main/java/ru/job4j/map/SimpleMap.java
@@ -0,0 +1,192 @@
+package ru.job4j.map;
+
+import java.util.*;
+
+public class SimpleMap<K, V> implements Map<K, V> {
+    private static final float LOAD_FACTOR = 0.75f;
+    private int capacity = 8;
+    private int count = 0;
+    private int countMod = 0;
+    private MapEntry<K, V>[] table = new MapEntry[capacity];
+
+    /**
+     * если кладем пару с ключем null - вызываем PutIfNullKey()
+     * иначе вычиляем бакет (index), если он пустой - кладем туда пару, count++
+     * если бакет занят, и ключи равны - перезатираем значение
+     * если бакет занят, и ключи не равны - возвращаем false
+     * в конце проверяем, не надо ли расширять массив
+     */
+    @Override
+    public boolean put(K key, V value) {
+        boolean rsl = false;
+        if (key == null) {
+            rsl = putIfNullKey(value);
+        } else {
+            int index = indexFor(key.hashCode());
+            if (table[index] == null) {
+                table[index] = new MapEntry<>(key, value);
+                count++;
+                countMod++;
+                rsl = true;
+            } else {
+                MapEntry<K, V> existedMapEntry = table[index];
+                if (key.equals(existedMapEntry.key)) {
+                    table[index] = new MapEntry<>(key, value);
+                    countMod++;
+                    rsl = true;
+                }
+            }
+        }
+        if (count / capacity >= LOAD_FACTOR) {
+            expand();
+        }
+        return rsl;
+    }
+
+    /**
+     * если бакет 0 пустой - увеличивает count на 1, затем кладет пару в бакет 0, true
+     * если бакет 0 уже содержит пару с ключом null - перезатирает значение, true
+     * если бакет 0 содержит пару с другим ключом - возвращает false
+     */
+    private boolean putIfNullKey(V value) {
+        boolean rsl = false;
+        if (table[0] == null) {
+            table[0] = new MapEntry<>(null, value);
+            count++;
+            countMod++;
+            rsl = true;
+        } else {
+            if (table[0].key == null) {
+                table[0] = new MapEntry<>(null, value);
+                countMod++;
+                rsl = true;
+            }
+        }
+        return rsl;
+    }
+
+    /**
+     * вычисляет индекс (номер бакета)
+     */
+    private int indexFor(int hashCode) {
+        int hash = hashCode ^ (hashCode >>> 16);
+        return hash % capacity;
+    }
+
+    /**
+     * расширяет массив table в два раза, кладет значения из старого массива в новый,
+     * при это рехешируя их (в методе put)
+     */
+    private void expand() {
+        capacity = capacity * 2;
+        count = 0;
+        MapEntry<K, V>[] oldTable = this.table;
+        this.table = new MapEntry[capacity];
+        for (MapEntry<K, V> mapEntry : oldTable) {
+            this.put(mapEntry.key, mapEntry.value);
+        }
+    }
+
+    /**
+     * если key == null - вызываем getIfNullKey(),
+     * иначе вычисляем номер бакета (index) и сравниваем ключи, если равны - возвращаем значение
+     * если бакет пустой или ключи не равны - возвращаем null
+     */
+    @Override
+    public V get(K key) {
+        V rsl = null;
+        if (key == null) {
+            rsl = getIfKeyNull();
+        } else {
+            int index = indexFor(key.hashCode());
+            if (table[index] != null && table[index].key.equals(key)) {
+                rsl = table[index].value;
+            }
+        }
+
+        return rsl;
+    }
+
+    /**
+     * если в бакете 0 лежит пара с ключом null - возвращаем значение,
+     * если бакет 0 пустой, или содержит пару с другим ключом - возвращаем null
+     */
+    private V getIfKeyNull() {
+        V rsl = null;
+        if (table[0] != null && table[0].key == null) {
+            rsl = table[0].value;
+        }
+        return rsl;
+    }
+
+    /**
+     * возвращает итератор
+     */
+    @Override
+    public Iterator iterator() {
+        return new Iterator<>() {
+            private int index = 0;
+            private int iteratorPoint = 0;
+            private final int expectedCountMod = countMod;
+
+            @Override
+            public boolean hasNext() {
+                return iteratorPoint < count;
+            }
+
+            @Override
+            public MapEntry<K, V> next() {
+                while (index < table.length && table[index] == null) {
+                    index++;
+                }
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                if (expectedCountMod != countMod) {
+                    throw new ConcurrentModificationException();
+                }
+                iteratorPoint++;
+                return table[index++];
+            }
+        };
+    }
+
+    /**
+     * вычисляет индекс переданного ключа
+     * и обнуляет ячейку массива с таким индексом, если ячейка не пуста и ключи идентичны
+     */
+    @Override
+    public boolean remove(K key) {
+        boolean rsl = false;
+        int index = indexFor(key.hashCode());
+        if (table[index] != null && table[index].key.equals(key)) {
+            table[index] = null;
+            count--;
+            countMod++;
+            rsl = true;
+        }
+        return rsl;
+    }
+
+    public int size() {
+        return count;
+    }
+
+    private static class MapEntry<K, V> {
+        K key;
+        V value;
+
+        public MapEntry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "MapEntry{" +
+                    "key=" + key +
+                    ", value=" + value +
+                    '}';
+        }
+    }
+}
---
+++ src/test/java/ru/job4j/map/SimpleMapTest.java
@@ -0,0 +1,145 @@
+package ru.job4j.map;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class SimpleMapTest {
+
+    @Test
+    public void putThreeDifferentPairs() {
+        SimpleMap<Integer, String> map = new SimpleMap<>();
+        map.put(1, "AAA");
+        map.put(2, "BBB");
+        assertThat(map.put(5, "CCC"), is(true));
+        assertThat(map.size(), is(3));
+    }
+
+    @Test
+    public void putTwoEquallyKeys() {
+        SimpleMap<Integer, String> map = new SimpleMap<>();
+        map.put(1, "AAA");
+        map.put(2, "BBB");
+        assertThat(map.put(2, "CCC"), is(true));
+        assertThat(map.size(), is(2));
+    }
+
+    @Test
+    public void putTwoNullKeys() {
+        SimpleMap<Integer, String> map = new SimpleMap<>();
+        map.put(null, "AAA");
+        map.put(12, "BBB");
+        assertThat(map.put(null, "CCC"), is(true));
+        assertThat(map.size(), is(2));
+    }
+
+    @Test
+    public void getNullKey() {
+        SimpleMap<Integer, String> map = new SimpleMap<>();
+        map.put(null, "AAA");
+        map.put(12, "BBB");
+        map.put(null, "CCC");
+        assertThat(map.get(null), is("CCC"));
+        assertThat(map.size(), is(2));
+    }
+
+    @Test
+    public void getNullKeyWhenNotExist() {
+        SimpleMap<Integer, String> map = new SimpleMap<>();
+        map.put(1, "AAA");
+        map.put(12, "BBB");
+        map.put(3, "CCC");
+        assertNull(map.get(null));
+    }
+
+    @Test
+    public void getKeyWhenNotExist() {
+        SimpleMap<Integer, String> map = new SimpleMap<>();
+        map.put(1, "AAA");
+        map.put(12, "BBB");
+        map.put(3, "CCC");
+        assertNull(map.get(5));
+    }
+
+    @Test
+    public void getKey() {
+        SimpleMap<Integer, String> map = new SimpleMap<>();
+        map.put(13, "AAA");
+        map.put(13, "BBB");
+        map.put(3, "CCC");
+        assertThat(map.get(13), is("BBB"));
+    }
+
+    @Test
+    public void putTenPairs() {
+        SimpleMap<Integer, String> map = new SimpleMap<>();
+        map.put(1, "AAA");
+        map.put(2, "BBB");
+        map.put(3, "CCC");
+        map.put(4, "DDD");
+        map.put(5, "EEE");
+        map.put(6, "FFF");
+        map.put(7, "GGG");
+        map.put(8, "HHH");
+        map.put(9, "III");
+        map.put(10, "JJJ");
+        assertThat(map.get(10), is("JJJ"));
+        assertThat(map.size(), is(10));
+    }
+
+    @Test
+    public void putAndRemove() {
+        SimpleMap<Integer, Character> map = new SimpleMap<>();
+        map.put(1, 'a');
+        map.put(2, 'b');
+        assertTrue(map.remove(1));
+        assertNull(map.get(1));
+        assertThat(map.get(2), is('b'));
+        assertThat(map.size(), is(1));
+    }
+
+    @Test
+    public void putAndRemoveWrong() {
+        SimpleMap<Integer, Double> map = new SimpleMap<>();
+        map.put(1, 6.8d);
+        assertFalse(map.remove(2));
+        assertThat(map.get(1), is(6.8d));
+        assertThat(map.size(), is(1));
+    }
+
+    @Test
+    public void getIterator() {
+        SimpleMap<Integer, String> map = new SimpleMap<>();
+        map.put(1, "A");
+        map.put(2, "B");
+        Iterator it = map.iterator();
+        assertTrue(it.hasNext());
+        it.next();
+        assertTrue(it.hasNext());
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void getIteratorWhenNoElements() {
+        SimpleMap<Integer, String> map = new SimpleMap<>();
+        map.put(1, "A");
+        Iterator it = map.iterator();
+        it.next();
+        it.next();
+    }
+
+    @Test
+    public void getElementWithIterator() {
+        SimpleMap<Integer, String> map = new SimpleMap<>();
+        map.put(1, "A");
+        Iterator it = map.iterator();
+        assertTrue(it.hasNext());
+        assertThat(it.next().toString(), is("MapEntry{" +
+                "key=" + 1 +
+                ", value=A" +
+                '}'));
+    }
+}
