bca958003fa8a1809dd67a7f5beaf5f8baaa9e61
Carcharodon80 <karna241280@gmail.com>
2021-09-17 00:32:54 +0300

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -3,17 +3,20 @@ package ru.job4j.io;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.function.Predicate;
 
 /**
  * 4.1. Сканирование файловой системы. [#106929]
+ * 5. Валидация параметров запуска. [#246865]
  */
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith("js")).forEach(System.out::println);
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Укажите начальную папку поиска и расширение нужных файлов");
+        }
+        Path start = Path.of(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
