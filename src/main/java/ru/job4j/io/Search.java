e3edc0b71c613e7b7375c3c083f84f48f1ee450a
Carcharodon80 <karna241280@gmail.com>
2021-09-19 23:36:41 +0300

    5. Валидация параметров запуска. [#246865 #204196]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -12,16 +12,22 @@ import java.util.function.Predicate;
  */
 public class Search {
     public static void main(String[] args) throws IOException {
-        if (args.length < 2) {
-            throw new IllegalArgumentException("Укажите начальную папку поиска и расширение нужных файлов");
+        if (isValid(args)) {
+            Path start = Path.of(args[0]);
+            search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
         }
-        Path start = Path.of(args[0]);
-        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    private static boolean isValid(String[] args) {
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Укажите начальную папку поиска и расширение нужных файлов");
+        }
+        return true;
     }
 }
