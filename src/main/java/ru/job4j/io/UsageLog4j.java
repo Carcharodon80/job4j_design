749922f79f9d1b0b106c230022ebf317d30027ca
Carcharodon80 <karna241280@gmail.com>
2021-10-17 01:12:30 +0300

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -6,15 +6,23 @@ import org.slf4j.LoggerFactory;
 /**
  * 1. Log4j. Логирование системы. [#95335]
  * 2. Simple Loggin Facade 4 Java. [#268849]
+ * 3. Slf4j - вывод переменных. [#268852]
  */
 public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        byte first = 1;
+        short second = 2000;
+        int third = 33;
+        long fourth = 44L;
+        float fifth = 55.5f;
+        double sixth = 66.6;
+        char seventh = '7';
+        boolean eighth = true;
+
+        LOG.debug("User info one : {}, two : {}, three : {}, four : {}, "
+                + "five : {}, six : {}, seven : {}, eight : {}", first, second,
+                third, fourth, fifth, sixth, seventh, eighth);
     }
 }
